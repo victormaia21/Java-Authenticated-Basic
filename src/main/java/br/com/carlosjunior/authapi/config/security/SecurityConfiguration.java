ca9abdfb1d2f009465dfc797c7341556b13811c6
Carlos Cazelatto Jr <carlosjunior1983@gmail.com>
2023-09-09 14:19:46 -0300

    Autenticação e Autorização com JWT

--- src/main/java/br/com/carlosjunior/authapi/config/security/SecurityConfiguration.java
+++ src/main/java/br/com/carlosjunior/authapi/config/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package br.com.carlosjunior.authapi.config.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -11,12 +12,16 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+    @Autowired
+    private SecurityFilter securityFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -33,6 +38,7 @@ public class SecurityConfiguration {
                         .requestMatchers(HttpMethod.POST,"/product").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
--- src/main/java/br/com/carlosjunior/authapi/infra/controllers/AuthorizationController.java
+++ src/main/java/br/com/carlosjunior/authapi/infra/controllers/AuthorizationController.java
@@ -1,39 +1,44 @@
 package br.com.carlosjunior.authapi.infra.controllers;
 
+import br.com.carlosjunior.authapi.config.security.TokenService;
 import br.com.carlosjunior.authapi.domain.dtos.AuthenticationDTO;
+import br.com.carlosjunior.authapi.domain.dtos.LoginResponseDTO;
 import br.com.carlosjunior.authapi.domain.dtos.UserRegisterDTO;
 import br.com.carlosjunior.authapi.domain.dtos.UserResponseDTO;
+import br.com.carlosjunior.authapi.domain.entities.User;
 import br.com.carlosjunior.authapi.domain.services.impl.AuthorizationService;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthorizationController {
 
-    private final AuthenticationManager authenticationManager;
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
-    private final AuthorizationService service;
+    @Autowired
+    private AuthorizationService service;
 
-    public AuthorizationController(AuthenticationManager authenticationManager,
-                                   AuthorizationService service) {
-        this.authenticationManager = authenticationManager;
-        this.service = service;
-    }
+    @Autowired
+    private TokenService tokenService;
 
     @PostMapping("/login")
-    public ResponseEntity<Void> login(@RequestBody @Valid AuthenticationDTO dto) {
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody @Valid AuthenticationDTO dto) {
         var usernamePassword = new UsernamePasswordAuthenticationToken(dto.getLogin(), dto.getPassword());
         var auth = this.authenticationManager.authenticate(usernamePassword);
-        return ResponseEntity.ok().build();
+        var token = tokenService.generateToken((User) auth.getPrincipal());
+        return new ResponseEntity<>(new LoginResponseDTO(token), OK);
     }
 
 
